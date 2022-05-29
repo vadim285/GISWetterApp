988c796ae9f2e5343952940f71dff73ef275a7b0
vadim285 <82446729+vadim285@users.noreply.github.com>
2022-05-29 13:43:42 +0200

    Server(work in Progress)

--- js/reg.js
+++ js/reg.js
@@ -6,12 +6,10 @@ mongoose.connect(dbURI)
     .then((result)=>console.log("Connected"));
 
 const User = require("./user");
-const name = document.querySelector(".Name");
-const password = document.querySelector(".Password");
-const region = document.querySelector(".Region");
 
 
 document.querySelector(".submit").addEventListener("click", function() {
     let data = { name: name, passwort: password, ort: region };
+    
 });
 
--- js/server.cjs
+++ js/server.cjs
@@ -1,22 +1,50 @@
 /* eslint-disable no-unused-vars */
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+const path = require("path");
+const port = 3000;
 
 
 const server = http.createServer((req, res)=>{
+    const uri = url.parse(req.url).pathname;
+
+    let path = "./";
+    switch (req.url) {
+    case "/":
+        path += "index.html";
+        res.statusCode = 200;
+        break;
+    case "/about.html":
+        path += "about.html";
+        res.statusCode = 200;
+        break;
+    case "/register.html":
+        path += "register.html";
+        res.statusCode = 200;
+        break;
+    case "/Ihr_Wetter.html":
+        path += "Ihr_Wetter.html";
+        res.statusCode = 200;
+        break;
+    default:
+        path += "index.html";
+        res.statusCode = 200;
+        break;
+    }
     res.setHeader("Content-Type", "text/html");
     // send an html file
 
-    fs.readFile("./index.html", (err, data)=>{
+    fs.readFile(path, (err, data)=>{
         if (err) {
             console.log(err);
             res.end();
         } else {
-            res.write(data);
-            res.end();
+            
+            res.end(data);
         }
     });
 });
-server.listen(3000, "localhost", ()=>{
+server.listen(port, "localhost", ()=>{
     console.log("listening for request");
 });
